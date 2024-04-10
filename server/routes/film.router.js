da3c3680e6b1aa6e3a3b77b4ec95c82dc32b6a6c
FedorKam <116003118+ArsAnik@users.noreply.github.com>
2024-04-10 22:20:43 +0400

    Update film.router.js

--- server/routes/film.router.js
+++ server/routes/film.router.js
@@ -5,11 +5,11 @@ const router = new Router();
 
 router.get('/getFilmFullInf/:id', FilmController.getFilmFullInf);
 router.get('/getFilmShortInf/:id', FilmController.getFilmShortInf);
-router.get('/getFilm/:genres/:actors/:director', FilmController.getFilm);
+// router.get('/getFilm/:genres/:actors/:director', FilmController.getFilm);
 
 router.get('/getGenres', FilmController.getGenres);
 router.get('/getActing', FilmController.getActing);
-router.get('/getActingInf', FilmController.getActingInf);
+router.get('/getActingInf/:id', FilmController.getActingInf);
 
 router.post('/addWantToWatch', FilmController.addWantToWatch);
 router.get('/getWantedFilms', FilmController.getWantedFilms);
