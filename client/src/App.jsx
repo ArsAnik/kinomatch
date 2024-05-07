551cd83b50c0f139b71e73308c4396104fa4c2be
FedorKam <116003118+ArsAnik@users.noreply.github.com>
2024-05-07 22:10:46 +0400

    front update styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {useRoutes} from "./routes.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
 function App() {
     const routes = useRoutes(false)
     return (
