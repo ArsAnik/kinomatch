e7c39bd85f11931f3e231bd17a0dff0dbbede781
Daria <daria180301@gmail.com>
2024-05-09 00:04:50 +0400

    front_fix

--- client/src/components/checkbox_filters/checkbox_filters.jsx
+++ client/src/components/checkbox_filters/checkbox_filters.jsx
@@ -3,9 +3,9 @@ import './checkbox_filters.css'
 
 const Checkbox_filters = (props) => {
     return (
-        <label className="filters_checkbox">
-            <input type="checkbox" checked={props.isChecked} onChange={()=> props.onChange((prev) => !prev)} className="checkbox_element"/>
-            <p>{props.children}</p>
+        <label className="filters_checkbox" for={props.name}>
+            <input type="checkbox" name={props.name} checked={props.isChecked} onChange={()=> props.onChange((prev) => !prev)} className="checkbox_element"/>
+            <p className="text_checkbox">{props.children}</p>
         </label>
 
 
--- client/src/pages/Filters/Filters.jsx
+++ client/src/pages/Filters/Filters.jsx
@@ -4,16 +4,17 @@ import Button_back from "../../components/button_back/button_back.jsx";
 import Checkbox_filters from "../../components/checkbox_filters/checkbox_filters.jsx";
 
 export const Filters =() =>{
-    const [rulesChecked,setRulesChecked] = useState(false);
+    const [rulesChecked, setRulesChecked] = useState(false);
     return(
         <div className="filters_body">
 
             <div className="filters_btn_block">
                 <Button_back/>
             </div>
+            <div className="filters_header">Фильтры</div>
             <div className="filters">
-                <div className="filters_header">Фильтры</div>
-                <Checkbox_filters isChecked={rulesChecked} onChange={setRulesChecked}>экшн</Checkbox_filters>
+                <Checkbox_filters name="экшн" isChecked={rulesChecked} onChange={setRulesChecked}>экшн</Checkbox_filters>
+                <Checkbox_filters name="боевик" isChecked={rulesChecked} onChange={setRulesChecked}>боевик</Checkbox_filters>
             </div>
 
         </div>
