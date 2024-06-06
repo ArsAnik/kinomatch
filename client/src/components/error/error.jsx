4cf193f448f067e771faa20dfe392d95b2a915c8
Daria <daria180301@gmail.com>
2024-06-06 06:34:37 +0400

    Fix main+error

--- client/src/components/error/error.jsx
+++ client/src/components/error/error.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './error.css'
+import Button_back from "../button_back/button_back.jsx";
 
 const Error = (props) => {
 
     return (
-        <div className="error">
-            <div className="name_error">{props.children}</div>
-            <div className="text_error">Упс... Произошла ошибка. Попробуйте ещё раз или обратитесь в поддержку xxxx@gmail.com</div>
+        <div>
+            <div className="btn_back_error">
+                <Button_back/>
+            </div>
+            <div className="error">
+
+                <div className="name_error">{props.children}</div>
+                <div className="text_error">Упс... Произошла ошибка. Попробуйте ещё раз или обратитесь в поддержку xxxx@gmail.com</div>
+            </div>
         </div>
+
 
     );
 };
